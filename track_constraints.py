84e6442c29d018b134cbfcc4a01518a382c07486
Binheng Zheng <bennhcheng@gmail.com>
2025-06-09 20:58:46 +0200

    Update track_constraints.py

--- track_constraints.py
+++ track_constraints.py
@@ -3,7 +3,7 @@ import numpy as np
 import casadi as ca
 import matplotlib.pyplot as plt
 
-# Numerical functions defining the track boundaries (Bezier-like)
+# Numerical functions defining the track boundaries (for visualization)
 def Pl_vis(x):
     h2 = 3.5
     if x <= 44: return 0
@@ -27,6 +27,30 @@ def Pu_vis(x):
     if x <= 95: return 4 * (h3 - h4) * (95 - x) ** 3 + h4
     return h4
 
+# CasADi-compatible expressions for symbolic computation
+def Pl_expr(x):
+    h2 = 3.5
+    return ca.if_else(x <= 44, 0,
+           ca.if_else(x <= 44.5, 4 * h2 * (x - 44)**3,
+           ca.if_else(x <= 45, 4 * h2 * (x - 45)**3 + h2,
+           ca.if_else(x <= 70, h2,
+           ca.if_else(x <= 70.5, 4 * h2 * (70 - x)**3 + h2,
+           ca.if_else(x <= 71, 4 * h2 * (71 - x)**3,
+           0))))))
+
+def Pu_expr(x):
+    B = 1.5
+    h1 = 1.1 * B + 0.25
+    h3 = 1.2 * B + 3.75
+    h4 = 1.3 * B + 0.25
+    return ca.if_else(x <= 15, h1,
+           ca.if_else(x <= 15.5, 4 * (h3 - h1) * (x - 15)**3 + h1,
+           ca.if_else(x <= 16, 4 * (h3 - h1) * (x - 16)**3 + h3,
+           ca.if_else(x <= 94, h3,
+           ca.if_else(x <= 94.5, 4 * (h3 - h4) * (94 - x)**3 + h3,
+           ca.if_else(x <= 95, 4 * (h3 - h4) * (95 - x)**3 + h4,
+           h4))))))
+
 class TrackConstraints:
     def __init__(self):
         self.B = 1.5
@@ -35,8 +59,8 @@ class TrackConstraints:
         X = ca.MX.sym('X', 2)
         x_pos, y_pos = X[0], X[1]
 
-        Pl = Pl_vis(x_pos) + self.B / 2
-        Pu = Pu_vis(x_pos) - self.B / 2
+        Pl = Pl_expr(x_pos) + self.B / 2
+        Pu = Pu_expr(x_pos) - self.B / 2
 
         g1 = Pl - y_pos  # Must be above lower boundary
         g2 = y_pos - Pu  # Must be below upper boundary
